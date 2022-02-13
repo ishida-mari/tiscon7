8a8145b90c1579c4d5819fc3256a5ec8d6132061
k-kogu <k.kogu1234@gmail.com>
2022-02-13 13:05:27 +0900

    add-post_code

--- src/main/java/com/tiscon/form/UserOrderForm.java
+++ src/main/java/com/tiscon/form/UserOrderForm.java
@@ -27,6 +27,12 @@ public class UserOrderForm {
     private String email;
 
     @NotBlank
+    private String zip1;
+
+    @NotBlank
+    private String zip2;
+
+    @NotBlank
     private String oldPrefectureId;
 
     @NotBlank
@@ -83,6 +89,22 @@ public class UserOrderForm {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getZip1() {
+        return zip1;
+    }
+
+    public void setZip1(String zip1) {
+        this.zip1 = zip1;
+    }
+
+    public String getZip2() {
+        return zip2;
+    }
+
+    public void setZip2(String zip2) {
+        this.zip2 = zip2;
     }
 
     public String getOldPrefectureId() {
